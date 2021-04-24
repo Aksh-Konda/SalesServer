d29310dcb3b1929599d80957913b126b8ca6bb83
akash konda <aksh.konda@gmail.com>
2021-04-24 19:40:58 +0530

    structured the code base

--- routes/index.js
+++ routes/index.js
@@ -6,45 +6,57 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   Footwear.find({})
     .then((fw) => {
-      res.render('layout', {
-        fw: fw,
-        state: 'sales'
-      });
+      const data = {
+        mainContent: {
+          location: './table',
+          data: {
+            fw: fw
+          }
+        },
+        currentRoute: 'sales'
+      };
+      res.render('layout', data);
     });
 });
 
 router.get('/sales', function (req, res, next) {
-  Footwear.find({})
-    .then((fw) => {
-      res.render('layout', {
-        fw: fw,
-        state: 'sales'
-      });
-    });
+  res.redirect('/');
 });
 
 router.get('/add', function (req, res, next) {
-  res.render('layout', {
-    state: 'add'
-  });
+  const data = {
+    mainContent: {
+      location: './addOrEdit.ejs',
+      data: {
+        op: 'add'
+      }
+    },
+    currentRoute: 'add'
+  }
+  res.render('layout', data);
 });
 
 router.get('/edit/:pid', function (req, res, next) {
   Footwear.findOne({ pid: req.params.pid })
     .then((fw) => {
-      res.render('layout', {
-        fw: fw,
-        state: 'edit'
-      });
+      const data = {
+        mainContent: {
+          location: './addOrEdit.ejs',
+          data: {
+            op: 'edit',
+            fw: fw
+          }
+        },
+        currentRoute: 'edit'
+      }
+      res.render('layout', data);
     });
 });
 
 router.get('/delete/:pid', function (req, res, next) {
   Footwear.deleteOne({ pid: req.params.pid })
     .then(resp => {
-      res.render('layout', {
-        state: 'deleted'
-      });
+      res.redirect('/');
     });
 });
 
@@ -52,18 +64,14 @@ router.post('/sales/add', function (req, res, next) {
   console.log(req.body);
   Footwear.create(req.body)
     .then(resp => {
-      res.render('layout', {
-        state: 'added'
-      });
+      res.redirect('/');
     });
 });
 
 router.post('/sales/edit', function (req, res, next) {
   Footwear.updateOne({ pid: req.body.pid }, { $set: req.body })
     .then(resp => {
-      res.render('layout', {
-        state: 'updated'
-      });
+      res.redirect('/');
     });
 });
 
